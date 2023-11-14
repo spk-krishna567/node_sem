bbf2e7bca43a7a901afcbea2a146ebd8f2be21da
spk-krishna567 <20191a0567jntucep@gmail.com>
2023-11-14 19:04:59 +0530

    by uma

--- index.js
+++ index.js
@@ -21,6 +21,39 @@ mongoose.connect("mongodb://0.0.0.0:27017/task").then(()=>{
             res.status(400).json({message:err.message})
         }
     })
+
+    app.get('/', async (req, res) => {
+        try {
+        const tasks = await Task.find();
+        res.json(tasks);
+        } catch (err) {
+        res.status(500).json({ message: err.message });
+        }})
+        app.put('/:id', async (req, res) => {
+
+            try{
+                const task = await Task.findById(req.params.id);
+                task.title = req.body.title;
+                task.description = req.body.description;
+                const updatedTask = await task.save();
+                res.status(201).json(updatedTask);
+            }catch(err){
+                res.status(400).json({ message: err.message });
+            }
+
+        });
+  
+    // Delete a task
+        app.delete('/delete-task/:id', async (req, res) => {
+            try{
+                await Task.deleteOne({_id: req.params.id});
+
+                res.status(201).json({message: "task deleted"});
+            }catch(err){
+                res.status(400).json({ message: err.message });
+            }
+        });
+
 })
 .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
@@ -28,4 +61,4 @@ mongoose.connect("mongodb://0.0.0.0:27017/task").then(()=>{
 
 app.listen(5001,()=>{
     console.log("sever is started successfully")
-});
+})
